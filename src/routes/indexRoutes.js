27077baec1e7c011e5762e3597524baaeb841e51
Robson Rodrigues <bobson278@gmail.com>
2022-07-07 23:06:47 -0300

    reoutes directory

---
+++ src/routes/indexRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        name: "cistech",
+        title: "Node Store API With Express js",
+        version: "0.0.1",
+    })
+})
